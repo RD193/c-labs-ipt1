c077b133b3160eda0867e6a518e7d49c5dc69696
RD193 <mihail.pol1999@yandex.ua>
2018-10-06 21:30:42 +0300

    Create 4_6.cpp

---
+++ lab 4/4_6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int* join(int q[], int r[]);
+int main() {
+  cout << "print length";
+  int n;//мне лень придумывать нормальные названия а потом печатать их каждый раз
+  int *c = new int[1];
+  cin >> n;
+  int a[n], b[n];
+  for(int i = 0; i < n*2; i = i + 2)
+  {
+    c[i] = 1;
+    c[i+1] = 2;
+  }
+  for(int i = 0; i < n*2; i++)
+  {
+    cout << c[i];
+  }
+  int *r = join(a,b);
+  for(int i = 0; i < n*2; i++)
+  {
+    cout << r[i];
+  }
+  delete [] c;
+
+}
+int* join(int *q, int *r) // Вне зависимости от динны и без всяких там динамических 
+{                         //массивов, жаль что не работает
+  int n = sizeof(q)/sizeof(*q);
+  cout << n;
+  int *resoult;
+  for(int i = 0; i < n; i = i + 2)
+  {
+    resoult[i] = 1;
+    resoult[i+1] = 2;
+  } 
+  return resoult;
+}
