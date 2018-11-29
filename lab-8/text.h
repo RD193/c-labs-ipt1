4d896faba1a6d8d359516bda453bc77fe21cd24a
RD193 <mihail.pol1999@yandex.ua>
2018-11-29 15:31:05 +0200

    Update text.h

--- lab-8/text.h
+++ lab-8/text.h
@@ -10,6 +10,6 @@ struct line
 line* Inp(const char* inp);
 void past(line* ln);
 int Count(line* ln);
-void change_letter(line*& ln, int line_number, int letter_number, char c);
-void swap_lines(line*& raw_ln, int i_line_num, int j_line_num);
-void change_line(line*& raw_ln, int i_line_num, int j_line_num);
+void change_letter(line* ln);
+void swap_lines(line* raw_ln);
+void change_line(line* raw_ln);
